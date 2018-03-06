fa668f877eb9d88df50e110b85d1e274c7162cf0
Firas <Firas@FirasMrabet>
2018-03-06 22:22:17 +0100

    Crud javafx Trader by Farah benslimene

---
+++ src/main/java/esprit/javaee/TestTraderService.java
@@ -0,0 +1,51 @@
+package esprit.javaee;
+
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import tn.esprit.thewolfs_server.entity.Level;
+import tn.esprit.thewolfs_server.entity.Trader;
+import tn.esprit.thewolfs_server.services.TraderServiceRemote;
+
+public class TestTraderService {
+
+	public TestTraderService() {
+		// TODO Auto-generated constructor stub
+	}
+  
+	public static void main(String[] args) throws NamingException {
+		String jndiname="thewolfs_server-ear/thewolfs_server-ejb/TraderService!tn.esprit.thewolfs_server.services.TraderServiceRemote";
+		Context context = new InitialContext();
+		TraderServiceRemote proxy=(TraderServiceRemote) context.lookup(jndiname);
+	   
+		//Ajout d'un Trader
+		/*Trader trader = new Trader("First", "Second", "First.Second@esprit.tn", "0000",Level.thirdLevel);
+	    System.out.println(proxy.addTrader(trader));/*
+		
+	    //Mise A jour d'un Trader
+	   /* Trader trader_update=new Trader();
+	    trader_update.setId();
+	    trader_update.setFirst_name("first_name");
+	    trader_update.setLast_name("last_name");
+	    trader_update.setEmail("email");
+	    trader_update.setPassword("password");
+	    trader_update.setLevel(Level.secondLevel);
+		System.out.println(proxy.updateTrader(trader_update));*/
+		
+		//Suppression d'un Trader
+		
+		
+		//Affichage d'un Trader
+		/*List<Trader> traders =proxy.dislayTrader();
+		for(Trader t: traders){
+			System.out.println(t);
+		}*/
+		
+	    //recherche d'une liste de trader
+		//System.out.println(proxy.findTraderByName("e"));
+	}
+	
+}
