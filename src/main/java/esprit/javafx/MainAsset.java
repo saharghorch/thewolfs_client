35ce2659841e5a6db67304a8d729e72134f3fd33
hafdhimanel <33426845+hafdhimanel@users.noreply.github.com>
2018-03-07 22:48:44 +0100

    Add files via upload
    javafx

---
+++ src/main/java/esprit/javafx/MainAsset.java
@@ -0,0 +1,36 @@
+
+package esprit.javafx;
+	
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class MainAsset extends Application {
+	@Override
+	  public void start(Stage stage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("AssetFXML.fxml"));
+            Scene scene = new Scene(root);
+            
+            stage.initStyle(StageStyle.TRANSPARENT);
+            
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+           
+        	Logger.getLogger(MainAsset.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+	
+   
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
